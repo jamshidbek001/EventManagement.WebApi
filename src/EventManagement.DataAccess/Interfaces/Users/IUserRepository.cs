45c655161dd579c3d3f2789909fb3bdb096a7652
jamshidbek001 <inomiddinovjamshidbek@gmail.com>
2023-11-02 09:27:55 +0100

    Finish repository interfaces

--- src/EventManagement.DataAccess/Interfaces/Users/IUserRepository.cs
+++ src/EventManagement.DataAccess/Interfaces/Users/IUserRepository.cs
@@ -1,8 +1,11 @@
 ﻿using EventManagement.DataAccess.Common.Interfaces;
+using EventManagement.DataAccess.ViewModels.Users;
 using EventManagement.Domain.Entities.Users;
 
 namespace EventManagement.DataAccess.Interfaces.Users;
 
 public interface IUserRepository :
-    IRepository<User,User>,IGetAll<User>
-{ }
+    IRepository<User,UserViewModel>,IGetAll<UserViewModel>,ISearchable<UserViewModel>
+{
+    public Task<User?> GetPhoneAsync(string phone);
+}
---
+++ src/EventManagement.DataAccess/ViewModels/Users/UserViewModel.cs
@@ -0,0 +1,5 @@
+﻿namespace EventManagement.DataAccess.ViewModels.Users;
+
+public class UserViewModel
+{
+}
